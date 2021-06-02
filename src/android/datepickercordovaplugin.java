bfb3a6c0d8d798969412f74ce50444aa531bd03b
louislim51 <louis.lim@orbistech.com>
2021-06-02 20:03:28 +0800

    import android log

--- src/android/datepickercordovaplugin.java
+++ src/android/datepickercordovaplugin.java
@@ -6,6 +6,7 @@ import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import android.util.Log;
 
 /**
  * This class echoes a string called from JavaScript.
